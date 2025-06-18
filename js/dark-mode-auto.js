a894a1eb781c077a35460e2c485526c1f0d7fd9d
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-06-18 15:32:58 -0400

    Added CSS + JS files. Added linter config.

---
+++ js/dark-mode-auto.js
@@ -0,0 +1,13 @@
+/**
+ * Automatically toggle dark mode based on users prefers-color-scheme OS setting
+ */
+(() => {
+
+	const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+	document.documentElement.classList.toggle('kelp-theme-dark', prefersDarkMode.matches);
+
+	prefersDarkMode.addEventListener('change', (event) => {
+		document.documentElement.classList.toggle('kelp-theme-dark', event.matches);
+	});
+
+})();
