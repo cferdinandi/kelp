10d4dbfe4f66ea0141ef7d271b49ebb2318ec174
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-07-07 02:32:49 -0400

    Added DOM selectors to tests

--- tests/toggle-pw/toggle-pw.spec.js
+++ tests/toggle-pw/toggle-pw.spec.js
@@ -8,30 +8,38 @@ const testPath = '/tests/toggle-pw';
 test.describe(`<${componentName}>`, () => {
 
 	testComponentReadyState(componentName, testPath);
-	testDebugEvent(testPath, 'toc-8');
+	testDebugEvent(testPath, 'wc-6', 'No password fields');
+	testDebugEvent(testPath, 'wc-7', 'No password toggle');
 
 	test('password is visible when checkbox is checked', async ({ page }) => {
 		await page.goto(testPath);
+		const checkbox = await page.getByTestId('toggle-1');
 	});
 
 	test('password visibility is toggled when button is pressed', async ({ page }) => {
 		await page.goto(testPath);
+		const button = await page.getByTestId('toggle-2');
 	});
 
 	test('password is visible by default when [visible] attribute is used', async ({ page }) => {
 		await page.goto(testPath);
+		const button = await page.getByTestId('toggle-3');
+		const input = await page.getByTestId('input-3');
 	});
 
 	test('[is-visible] and [is-hidden] attributes conditionally show content', async ({ page }) => {
 		await page.goto(testPath);
+		const button = await page.getByTestId('toggle-3');
 	});
 
 	test('.toggle(), .show(), and .hide() methods work', async ({ page }) => {
 		await page.goto(testPath);
+		const wc = await page.getByTestId('wc-4');
 	});
 
 	test('[aria-pressed] is added to button and toggled based on state', async ({ page }) => {
 		await page.goto(testPath);
+		const button = await page.getByTestId('toggle-5');
 	});
 
 });
