ad75c096c0ccd1cdc2075c77988b58da8d1778a9
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-07-10 17:25:12 -0400

    Added missing JS file

--- js/kelp.js
+++ js/kelp.js
@@ -2,3 +2,4 @@ import './components/toc.js';
 import './components/toc-nested.js';
 import './components/heading-anchors.js';
 import './components/toggle-pw.js';
+import './components/autogrow.js';
