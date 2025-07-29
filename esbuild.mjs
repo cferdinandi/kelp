33a5652b4f47c2aad9c76c2f47d0ea49e88096de
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-07-28 22:38:43 -0400

    bundle for release

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,28 +1,15 @@
 import * as esbuild from 'esbuild';
-import { glob } from 'glob';
-import pkg from './package.json' with { type: "json" };
+import pkg from './package.json' with { type: 'json' };
 
 const banner = `/*! ${pkg.name} v${pkg.version} | (c) ${pkg.author.name} | ${pkg.repository.url} */`;
 
-// Get all the modular files
-const files = await glob([
-	'modules/js/*.js',
-	'modules/js/components/*.js',
-	'modules/css/*.css'
-]);
-
-// Generate the input/output object for the files
-const entryPoints = files.map((file) => {
-	const output = file.replace('modules/', '').replace('.js', '').replace('.css', '');
-	return {
-		in: file,
-		out: output,
-	};
-});
-
-// Build the files
 await esbuild.build({
-	entryPoints,
+	entryPoints: [
+		'modules/js/*.js',
+		'modules/js/components/*.js',
+		'modules/css/*.css',
+	],
+	outbase: 'modules',
 	outdir: './',
 	banner: {
 		js: banner,
