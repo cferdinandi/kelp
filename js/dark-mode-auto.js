3d36a96bee46f14594acb472679a57a4daae841b
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-08-16 15:40:19 -0400

    Cascade Layers (#203)
    * Updated cascade layer order, setup, and names. Switched .kelp-theme-* classes to just .dark and .light.
    
    * Removed unused file
    
    * bundle for release

--- js/dark-mode-auto.js
+++ js/dark-mode-auto.js
@@ -1,10 +1,10 @@
-/*! kelpui v0.19.0 | (c) Chris Ferdinandi | http://github.com/cferdinandi/kelp */
+/*! kelpui v0.20.0 | (c) Chris Ferdinandi | http://github.com/cferdinandi/kelp */
 "use strict";
 (() => {
   // modules/js/dark-mode-auto.js
   var prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-  document.documentElement.classList.toggle("kelp-theme-dark", prefersDarkMode.matches);
+  document.documentElement.classList.toggle("dark", prefersDarkMode.matches);
   prefersDarkMode.addEventListener("change", (event) => {
-    document.documentElement.classList.toggle("kelp-theme-dark", event.matches);
+    document.documentElement.classList.toggle("dark", event.matches);
   });
 })();
--- modules/js/dark-mode-auto.js
+++ modules/js/dark-mode-auto.js
@@ -2,8 +2,8 @@
  * Automatically toggle dark mode based on users prefers-color-scheme OS setting
  */
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-document.documentElement.classList.toggle('kelp-theme-dark', prefersDarkMode.matches);
+document.documentElement.classList.toggle('dark', prefersDarkMode.matches);
 
 prefersDarkMode.addEventListener('change', (event) => {
-	document.documentElement.classList.toggle('kelp-theme-dark', event.matches);
+	document.documentElement.classList.toggle('dark', event.matches);
 });
