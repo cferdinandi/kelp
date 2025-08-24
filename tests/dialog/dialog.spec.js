f762b7d440ed9ed2214de2ab0104b118ebc67991
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-08-23 23:44:33 -0400

    WIP

---
+++ tests/dialog/dialog.spec.js
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+import { testComponentReadyState, waitForCustomEvent } from '../test-utilities.js';
+
+// Component details
+const componentName = 'kelp-dialog';
+const testPath = '/tests/dialog';
+
+
+test.describe(`<${componentName}>`, () => {
+
+	testComponentReadyState(componentName, testPath);
+
+	test('default component', async ({ page }) => {
+
+		await page.goto(`${testPath}/default.html`);
+
+		// Get elements
+		const wc = page.locator(componentName);
+
+		// Clicking the button shows the modal
+
+		// Clicking a button inside the modal closes it
+
+		// Closing the modal returns focus to the trigger button
+
+		// open event emits on the dialog
+
+	});
+
+	test('error handling', async ({ page }) => {
+
+		await page.goto(`${testPath}/errors.html`);
+
+		// Get elements
+		const wc = page.locator(componentName);
+
+		// If there's no target provided, the component doesn't instantiate
+
+		// If there's no matching dialog, the component doesn't instantiate
+
+	});
+
+});
