c063d2fad93dc2e419dbd9a36c9a8eadd8f8a019
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-08-28 00:12:36 -0400

    Odds and Ends (part 2) (#228)
    * Added tests. Added display classes. Changed .btn-link to .link.
    
    * bundle for release

---
+++ tests/hide-until-ready/hide-until-ready.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+// Component details
+const componentName = 'hide-until-ready';
+const testPath = '/tests/hide-until-ready';
+
+test.describe(`[${componentName}]`, () => {
+
+	test('default behavior', async ({ page }) => {
+
+		await page.goto(`${testPath}/default.html`);
+
+		// Get elements
+		const wc = page.locator('kelp-fake-component');
+
+		// Web component should be hidden
+		await expect(wc).not.toBeVisible();
+
+		// Once ready, web component should be visible
+		await wc.evaluate((elem) => elem.setAttribute('is-ready', ''));
+		await expect(wc).toBeVisible();
+
+	});
+
+});
