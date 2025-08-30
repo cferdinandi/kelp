395c7f32ac67dc96990c0731dac5d263c0ef8aa7
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-08-29 23:08:09 -0400

    bundle for release

--- js/dark-mode-auto.js
+++ js/dark-mode-auto.js
@@ -1,4 +1,4 @@
-/*! kelpui v1.5.0 | (c) Chris Ferdinandi | http://github.com/cferdinandi/kelp */
+/*! kelpui v1.5.1 | (c) Chris Ferdinandi | http://github.com/cferdinandi/kelp */
 "use strict";
 (() => {
   // modules/js/dark-mode-auto.js
