8fbc29b6ba6461505f9f28623e458d1d62cc2670
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-08-31 13:02:47 -0400

    Top & Bottom Drawers (#233)
    * Added top/bottom drawers and size modifiers
    
    * bundle for release

--- modules/js/utilities/getFilteredSelector.js
+++ modules/js/utilities/getFilteredSelector.js
@@ -1,10 +1,8 @@
-const exclude = ['kelp-tabs', 'kelp-accordion', 'dialog'];
-
 /**
  * Get a selector string with certain web components filtered out
  * @param  {String} selector The original selector string
  * @return {String}          The filtered version of the string
  */
 export function getFilteredSelector (selector) {
-	return `${selector}:not(${exclude.map(elem => `${elem} ${selector}`)}) `;
+	return `${selector}:not(:is(kelp-tabs, kelp-accordion, dialog) ${selector}) `;
 }
