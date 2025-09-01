f54a1181342b299c908aa13f2a9f5d0a92ddb189
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2025-09-01 01:25:41 -0400

    Fix temporal styling

---
+++ tests/temporal-inputs/temporal-inputs.spec.js
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test';
+
+// Component details
+const componentName = 'temporal inputs';
+const testPath = '/tests/temporal-inputs';
+
+test.describe(componentName, () => {
+
+	test('should match size of text inputs', async ({ page }) => {
+
+		await page.goto(`${testPath}/default.html`);
+
+		// Elements
+		const text = page.locator('#text').first();
+		const date = page.locator('#date').first();
+		const time = page.locator('#time').first();
+		const datetime = page.locator('#datetime-local').first();
+
+		// Dimensions
+		const textXY = await text.evaluate(elem => {
+			const dimensions = window.getComputedStyle(elem);
+			return {
+				width: dimensions.width,
+				height: dimensions.height,
+			};
+		});
+		const dateXY = await date.evaluate(elem => {
+			const dimensions = window.getComputedStyle(elem);
+			return {
+				width: dimensions.width,
+				height: dimensions.height,
+			};
+		});
+		const timeXY = await time.evaluate(elem => {
+			const dimensions = window.getComputedStyle(elem);
+			return {
+				width: dimensions.width,
+				height: dimensions.height,
+			};
+		});
+		const datetimeXY = await datetime.evaluate(elem => {
+			const dimensions = window.getComputedStyle(elem);
+			return {
+				width: dimensions.width,
+				height: dimensions.height,
+			};
+		});
+
+		// Height and width of temporals should match text input
+		await expect(dateXY).toEqual(textXY);
+		await expect(timeXY).toEqual(textXY);
+		await expect(datetimeXY).toEqual(textXY);
+
+	});
+
+});
